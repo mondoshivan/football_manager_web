58b75e0109679ba08bd9542259d7c76fb489bad9
Oliver Schmidt <o.schmidt@steinberg.de>
2022-10-09 09:23:09 +0200

    [update] front-end dependency in server-rest-api

--- packages/server-rest-api/src/config/config_service.ts
+++ packages/server-rest-api/src/config/config_service.ts
@@ -4,7 +4,7 @@ export class ServiceConfig implements config.ServiceConfig {
 
     @Property({
         doc: 'The location of the front-end.',
-        default: "node_modules/@sequencer-localizator-webservice/server-front-end/dist/server-front-end",
+        default: "node_modules/@football_manager/front-end/dist/front-end",
         env: 'SLW_SERVICE_FRONT_END_DIR',
     })
     public frontEndDir!: string;
