6c47284929ac8374278552cb2c2f661630274c0d
Oliver Schmidt <o.schmidt@steinberg.de>
2022-10-14 13:09:59 +0200

    [update] async handler added

--- packages/server-rest-api/src/routers/v1/club.ts
+++ packages/server-rest-api/src/routers/v1/club.ts
@@ -1,23 +1,21 @@
 import { Request, Response, Router } from "express";
+import asyncHandler from "express-async-handler"
+
 import { CreateClubDTO } from "../../data-transfer-objects/v1/club.dto";
 
 import { clubService } from "@football-manager/db-handler"
 
 const clubRouter = Router();
 
-clubRouter.get('/', async (req: Request, res: Response) => {
-    res.status(200).json({ name: 'HSV' });
-})
+clubRouter.get('/', asyncHandler( async (req: Request, res: Response) => {
+    const result = await clubService.getAll({});
+    res.status(200).json(result);
+}));
 
-clubRouter.post('/', async (req: Request, res: Response) => {
-    const payload:CreateClubDTO = req.body
-
-    try {
-        const result = await clubService.create(payload)
-        return res.status(200).send(result)
-    } catch (error) {
-        res.status(500).send('failed')
-    } 
-})
+clubRouter.post('/', asyncHandler( async (req: Request, res: Response) => {
+    const payload:CreateClubDTO = req.body;
+    const result = await clubService.create(payload);
+    res.status(200).send(result);
+}));
 
 export default clubRouter;
