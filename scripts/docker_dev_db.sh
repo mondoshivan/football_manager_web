3607d35566ae39a941c1162c4f66e2720a5704e9
Oliver Schmidt <o.schmidt@steinberg.de>
2022-10-20 15:22:29 +0200

    [update] remove docker volume

--- scripts/docker_dev_db.sh
+++ scripts/docker_dev_db.sh
@@ -14,14 +14,13 @@ cd $(dirname "$0")/..
 
 # run db
 docker run -d --name fm_dev_db \
-    -e MARIADB_ROOT_PASSWORD=${FM_DB_ROOT_PASSWORD} \
-    -e MARIADB_USER=${FM_DB_USER} \
-    -e MARIADB_PASSWORD=${FM_DB_PASSWORD} \
-    -e MARIADB_DATABASE=${FM_DB_NAME} \
+    -e MARIADB_ROOT_PASSWORD="${FM_DB_ROOT_PASSWORD}" \
+    -e MARIADB_USER="${FM_DB_USER}" \
+    -e MARIADB_PASSWORD="${FM_DB_PASSWORD}" \
+    -e MARIADB_DATABASE="${FM_DB_NAME}" \
     -p 3307:3306 \
     -v ${PWD}/db/dev/mysqlconf:/etc/mysql/conf.d:delegated \
     -v ${PWD}/db/dev/initialize:/docker-entrypoint-initdb.d:delegated \
-    -v ${FM_HOME}/environments/dev/data:/var/lib/mysql:delegated \
     --rm \
     mariadb:latest
 
